4b483038784f4b5893a086d387714a6aaa21815e
Guillermo Quiros <gquiman@gmail.com>
2023-11-15 13:22:43 +0100

    Up version

--- flux/fullCICD/app1/src/server.js
+++ flux/fullCICD/app1/src/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello App version 1.0.0.0");
+  res.send("Hello App version New one");
 });
 
 app.listen(port, () => console.log(`The server is listening on port ${port}`));
