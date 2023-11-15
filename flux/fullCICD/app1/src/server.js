178f8a89b9216af0b96752b64db9dd0586d6ccb5
Guillermo Quiros <gquiman@gmail.com>
2023-11-15 13:33:38 +0100

    Adding version

--- flux/fullCICD/app1/src/server.js
+++ flux/fullCICD/app1/src/server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 8000;
 
 app.get("/", (req, res) => {
-  res.send("Hello App version New one");
+  res.send("Hello App version New two");
 });
 
 app.listen(port, () => console.log(`The server is listening on port ${port}`));
